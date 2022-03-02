57beb6fa52030d1d83a541c6d511145167704ef5
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-02 19:44:07 +0530

    Added

--- index.js
+++ index.js
@@ -1,24 +1,69 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const authRoute = require("./Routes/auth");
 dotenv.config();
-app.use(express.json());
 
+const URL =
+  "mongodb+srv://sakthi123:sakthi123@cluster0.jvpny.mongodb.net/blog?retryWrites=true&w=majority";
+// const cors = require("cors");
+// const multer = require("multer");
+// const path = require("path");
+
+//routes
+const authRoute = require("./Routes/auth");
+// const userRoute = require("./Routes/Users");
+// const postRoute = require("./Routes/Posts");
+// const categoryRoute = require("./Routes/Categories");
+// app.use(cors());
+app.use(express.json());
+// app.use("/images", express.static(path.join(__dirname, "/images")));
+// app.use("/images", express.static(path.join(__dirname, "/images")));
+const mongoose = require("mongoose");
 mongoose
-  .connect(process.env.MONGODB_URL, {
+  .connect(URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
+    useFindAndModify: true,
   })
-  .then(console.log("mongodb connected successfully"))
-  .catch((err) => console.log("err", err.message));
-app.get("/", (req, res) => {
-  res.status(200).send("server running ");
-});
-app.use("/api/auth", authRoute);
+  .then(console.log("connected to mongo db"))
+  .catch((err) => console.log("Error in Db connection", err));
 
-app.listen("5000", () => {
-  console.log("backend is running");
+//image multer
+//storage file location choosing;
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "images");
+//   },
+//   filename: (req, file, cb) => {
+//     console.log(req.body);
+//     cb(null, file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+// // make public folder:
+
+// app.post("/api/upload", upload.single("file"), (req, res) => {
+//   try {
+//     console.log("in upload", req.file);
+//     console.log("In image upload");
+//     res.status(200).send("file has been uploaded");
+//   } catch (err) {
+//     res.status(500).send("error in uploading");
+//   }
+// });
+
+//middlewares
+
+app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
+// app.use("/api/posts", postRoute);
+// app.use("/api/categories", categoryRoute);
+
+port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log("server running at ", port);
 });
